61361e4ee5d1f43934c5a237aa6bc906ce54e7f1
Mikael Shumacher <mikaelabrell50@gmail.com>
2025-07-09 10:39:50 -0300

    int main

---
+++ mathC.c
@@ -0,0 +1,13 @@
+//
+// Created by shumacher on 7/9/25.
+//
+#include <stdio.h>
+#include "mathC.h"
+
+int main(){
+
+    return 0;
+}
+
+
+
---
+++ mathC.h
@@ -0,0 +1,8 @@
+//
+// Created by shumacher on 7/9/25.
+//
+
+#ifndef MATHC_H
+#define MATHC_H
+
+#endif //MATHC_H
