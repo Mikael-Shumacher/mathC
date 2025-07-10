39c6793ba829fa826eba989cd3f2d69f27eb84f8
Mikael-Shumacher <mikaelabrell50@gmail.com>
2025-07-10 20:06:36 -0300

    funcao raiz2

--- mathC.c
+++ mathC.c
@@ -4,8 +4,32 @@
 #include <stdio.h>
 #include "mathC.h"
 
-int main(){
 
+void raiz2 (float num){
+	float result = 0;
+	int cont = 1;
+	while((result*result) != num){
+		if((result * result) > num){
+			result = 0;
+			cont = cont/10; 
+		}
+		if(num < 0){
+			printf("Não existe raiz quadrada de número negativo, no conjunto dos Reais");
+			break;
+		}
+		result += cont;
+		printf("%f\n", result);
+	}
+	printf("\n%f\n", result);
+}
+
+
+
+int main(){
+	float num1;
+	printf("Digite um número para calcularmos a raiz quadrada: ");
+	scanf("%f", &num1);
+	raiz2(num1);
     return 0;
 }
 
