cd92cfdfd8f2eebed4b2cf9e770bb83a6202618f
Mikael Shumacher <mikaelabrell50@gmail.com>
2025-07-11 23:36:39 -0300

    eleva

--- mathC.c
+++ mathC.c
@@ -24,12 +24,32 @@ void raiz2 (float num){
 }
 
 
+float eleva(float num, int pot){
+	float num2 = 1;
+	if(pot == 1)
+		return num;
+	else if (pot ==0)
+		return num2;
+	else if (pot > 1){
+		for(int i = 0; i < pot; i++){
+			num2 = num2 * num;
+		}
+		return num2;
+	}
+}
+
 
 int main(){
-	float num1;
+	/*float num1;
 	printf("Digite um número para calcularmos a raiz quadrada: ");
 	scanf("%f", &num1);
 	raiz2(num1);
+	*/
+	float num1;
+	int exp;
+	printf("Digite um número e o expoente: ");
+	scanf("%f %d", &num1, &exp);
+	printf("\n%f\n", eleva(num1, exp));
     return 0;
 }
 
